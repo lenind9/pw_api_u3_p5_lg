7e040f2725f6f065cf60d93e6d6c621606dab9fd
lenind9 <lenindgc@hotmail.com>
2024-01-27 10:48:36 -0500

    Verbo GET y POST en Postman

--- src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
+++ src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
@@ -1,17 +1,35 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.repository.modelo.Estudiante;
+import com.example.demo.service.IEstudianteService;
 
 // API: determinada por el proyecto java
 // Servicio -> Controller: clase Controller
 
 @RestController // Servicio
+@RequestMapping(path = "/estudiantes")
 public class EstudianteControllerRestFul {
 	
+	@Autowired
+	private IEstudianteService estudianteService;
+	
+	// Verbo: GET
+	@GetMapping(path = "/buscar")
+	public Estudiante buscar() {
+		return this.estudianteService.buscar(1);
+	}
+	// http://localhost:8080/API/v1.0/Matricula/estudiantes/buscar
+	
 	// Metodos: Capacidades
-	public void guardar(Estudiante estudiante) {
-		
+	@PostMapping(path = "/guardar")
+	public void guardar(@RequestBody Estudiante estudiante) {
+		this.estudianteService.guardar(estudiante);
 	}
 }
