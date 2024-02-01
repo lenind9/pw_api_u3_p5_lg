5534ef43180546a28b982c1208ada7eb5c895cd8
Lenin Guananga <lenindgc@hotmail.com>
2024-01-31 19:43:29 -0500

    Actualizar, parcial y borrar

--- src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
+++ src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +23,8 @@ public class EstudianteControllerRestFul {
 	@Autowired
 	private IEstudianteService estudianteService;
 	
+	// Metodos: Capacidades
+	
 	// Verbo: GET
 	@GetMapping(path = "/buscar")
 	public Estudiante buscar() {
@@ -27,9 +32,23 @@ public class EstudianteControllerRestFul {
 	}
 	// http://localhost:8080/API/v1.0/Matricula/estudiantes/buscar
 	
-	// Metodos: Capacidades
 	@PostMapping(path = "/guardar")
 	public void guardar(@RequestBody Estudiante estudiante) {
 		this.estudianteService.guardar(estudiante);
+	}
+	
+	@PutMapping(path = "/actualizar")
+	public void actualizar(@RequestBody Estudiante estudiante) {
+	    this.estudianteService.actualizar(estudiante);
+	}
+	
+	@PatchMapping(path = "/actualizarParcial")
+	public void actualizarParcial(@RequestBody Estudiante estudiante) {
+		this.estudianteService.actualizarParcial(estudiante.getApellido(), estudiante.getNombre(), estudiante.getId());
+	}
+	
+	@DeleteMapping(path = "/borrar")
+	public void borrar() {
+		this.estudianteService.borrar(4);
 	}
 }
